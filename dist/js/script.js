8c83ad8f78698a4a597af6a012d056780c328ebe
Sanya <example@mail.ru>
2020-04-02 21:36:06 +0300

    second home-task

---
+++ dist/js/script.js
@@ -0,0 +1,38 @@
+let money = +prompt("Ваш бюджет на месяц?", '');
+money;
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+let appData = {
+    budjet: money,
+    timeData: time,
+    expenses = {},
+    optionalExpenses = {},
+    income = [],
+    saving = false
+
+
+}
+
+for(i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    if ( (typeof(a)) === 'string' && (typeof(a) != null ) && (typeof(b) != null ) && a != '' && b != '' && a.length < 50){
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+
+    }
+};
+
+appData.moneyPerDay = appData.budjet / 30;
+
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if( appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка")
+} else if( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка")ж
+} else if( appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка")
+}
